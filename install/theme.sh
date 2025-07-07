da60b727ec1a5b583caaf90f9d8e31d39ce1a114
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2025-07-07 17:35:30 -0400

    fixes

--- install/theme.sh
+++ install/theme.sh
@@ -5,3 +5,6 @@ sudo pacman -S --noconfirm kvantum-qt5
 sudo pacman -S --noconfirm gnome-themes-extra # Adds Adwaita-dark theme
 gsettings set org.gnome.desktop.interface gtk-theme "Adwaita-dark"
 gsettings set org.gnome.desktop.interface color-scheme "prefer-dark"
+
+# Set initial background
+ln -snf ~/.config/theme/backgrounds/background1.jpg ~/.config/theme/background
