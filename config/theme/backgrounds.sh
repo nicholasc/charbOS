5d62c7b1320e18151fc53579f9377a2a85997ed0
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2025-07-07 16:07:25 -0400

    fix: background path

--- config/theme/backgrounds.sh
+++ config/theme/backgrounds.sh
@@ -1,1 +1,1 @@
-download_background_image "https://github.com/basecamp/omakub/raw/refs/heads/master/themes/gruvbox/background.jpg" "gruvbox/1-grubox.jpg"
+download_background_image "https://github.com/basecamp/omakub/raw/refs/heads/master/themes/gruvbox/background.jpg" "background1.jpg"
