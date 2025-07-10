84710c5e76ac73106a2338314cd8c9d8040a55d1
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2025-07-10 19:28:37 -0400

    fix: typo

--- install/core/bluetooth.sh
+++ install/core/bluetooth.sh
@@ -4,4 +4,4 @@ _ yay -S --noconfirm --needed blueberry
 
 # Turn on bluetooth by default
 gum log --level info "Turning on bluetooth by default..."
-_ sudo systemctl enable --now bluetooth.kervice
+_ sudo systemctl enable --now bluetooth.service
