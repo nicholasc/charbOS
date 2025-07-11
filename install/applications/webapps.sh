8d3d8931baac95523dd6a74b519e00a8f4c636a2
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2025-07-11 11:15:52 -0400

    feat: basic webapps

--- install/applications/webapps.sh
+++ install/applications/webapps.sh
@@ -1,4 +1,7 @@
 source ~/.charbOS/default/bash/functions
 
 # https://dashboardicons.com/
-#web2app "WhatsApp" https://web.whatsapp.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/whatsapp.png
+web2app "Messenger" https://messenger.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/facebook-messenger.png
+web2app "Mail" https://mail.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/gmail.png
+web2app "Calendar" https://calendar.google.com/ https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-calendar.png
+web2app "Gemini" https://gemini.google.com/gem/3c65c0facd52 https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/google-gemini.png
