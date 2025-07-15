806ce82770d6663c6b8447de481ef3f1891d5ee8
Nicholas Charbonneau <nicholas.charbonneau@gmail.com>
2025-07-15 11:04:39 -0400

    fix: updating cursor location

--- install/applications/cursor.sh
+++ install/applications/cursor.sh
@@ -1,5 +1,5 @@
 gum log --level info "Installing Cursor..."
 
 wget https://os.charbs.dev/apps/Cursor.AppImage
-sudo mv Cursor.AppImage /usr/local/bin/cursor
-sudo chmod +x /usr/local/bin/cursor
+mv Cursor.AppImage ~/.local/bin/cursor
+chmod +x ~/.local/bin/cursor
--- install/applications/main.sh
+++ install/applications/main.sh
@@ -1,3 +1,6 @@
+# Create a directory for local binaries
+mkdir -p ~/.local/bin
+
 # Install applications
 items=("cursor" "webapps" "xtras")
 install_package "applications" "${items[@]}"
